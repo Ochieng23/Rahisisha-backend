ebf69e894f56a3ee9bc7e4f84e3e9bd5bb858f31
margaret manyeki <maggiemanyeki6@gmail.com>
2023-04-17 22:38:09 +0300

    updated gems

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,101 +10,102 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 20_230_415_203_025) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_15_203025) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'admins', force: :cascade do |t|
-    t.string 'admin_code'
-    t.string 'full_name'
-    t.string 'email'
-    t.string 'user_code'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['admin_code'], name: 'index_admins_on_admin_code', unique: true
+  create_table "admins", force: :cascade do |t|
+    t.string "admin_code"
+    t.string "full_name"
+    t.string "email"
+    t.string "user_code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["admin_code"], name: "index_admins_on_admin_code", unique: true
   end
 
-  create_table 'comments', force: :cascade do |t|
-    t.string 'comment_code'
-    t.string 'content'
-    t.string 'post_code'
-    t.string 'user_code'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['comment_code'], name: 'index_comments_on_comment_code', unique: true
+  create_table "comments", force: :cascade do |t|
+    t.string "comment_code"
+    t.string "content"
+    t.string "post_code"
+    t.string "user_code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["comment_code"], name: "index_comments_on_comment_code", unique: true
   end
 
-  create_table 'employers', force: :cascade do |t|
-    t.string 'employer_code'
-    t.string 'company_name'
-    t.string 'email'
-    t.string 'location'
-    t.string 'avatar'
-    t.string 'description'
-    t.boolean 'verified'
-    t.string 'user_code'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['employer_code'], name: 'index_employers_on_employer_code', unique: true
+  create_table "employers", force: :cascade do |t|
+    t.string "employer_code"
+    t.string "company_name"
+    t.string "email"
+    t.string "location"
+    t.string "avatar"
+    t.string "description"
+    t.boolean "verified"
+    t.string "user_code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["employer_code"], name: "index_employers_on_employer_code", unique: true
   end
 
-  create_table 'jobs', force: :cascade do |t|
-    t.string 'job_code'
-    t.string 'job_name'
-    t.string 'description'
-    t.string 'employer_code'
-    t.string 'jobtag_code'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['job_code'], name: 'index_jobs_on_job_code', unique: true
+  create_table "jobs", force: :cascade do |t|
+    t.string "job_code"
+    t.string "job_name"
+    t.string "description"
+    t.string "employer_code"
+    t.string "jobtag_code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["job_code"], name: "index_jobs_on_job_code", unique: true
   end
 
-  create_table 'jobtags', force: :cascade do |t|
-    t.string 'jobtag_code'
-    t.string 'group_name'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['jobtag_code'], name: 'index_jobtags_on_jobtag_code', unique: true
+  create_table "jobtags", force: :cascade do |t|
+    t.string "jobtag_code"
+    t.string "group_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["jobtag_code"], name: "index_jobtags_on_jobtag_code", unique: true
   end
 
-  create_table 'posts', force: :cascade do |t|
-    t.string 'post_code'
-    t.string 'title'
-    t.string 'media'
-    t.string 'description'
-    t.integer 'likes'
-    t.string 'user_code'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['post_code'], name: 'index_posts_on_post_code', unique: true
+  create_table "posts", force: :cascade do |t|
+    t.string "post_code"
+    t.string "title"
+    t.string "media"
+    t.string "description"
+    t.integer "likes"
+    t.string "user_code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_code"], name: "index_posts_on_post_code", unique: true
   end
 
-  create_table 'seekers', force: :cascade do |t|
-    t.string 'seeker_code'
-    t.string 'full_name'
-    t.string 'email'
-    t.string 'location'
-    t.string 'gender'
-    t.string 'date_of_birth'
-    t.string 'avatar'
-    t.string 'phone_number'
-    t.string 'preferred_job'
-    t.string 'availability'
-    t.integer 'minimum_salary'
-    t.boolean 'verified'
-    t.string 'user_code'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['seeker_code'], name: 'index_seekers_on_seeker_code', unique: true
+  create_table "seekers", force: :cascade do |t|
+    t.string "seeker_code"
+    t.string "full_name"
+    t.string "email"
+    t.string "location"
+    t.string "gender"
+    t.string "date_of_birth"
+    t.string "avatar"
+    t.string "phone_number"
+    t.string "preferred_job"
+    t.string "availability"
+    t.integer "minimum_salary"
+    t.boolean "verified"
+    t.string "user_code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["seeker_code"], name: "index_seekers_on_seeker_code", unique: true
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'user_code'
-    t.string 'username'
-    t.string 'password_digest'
-    t.string 'role'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['user_code'], name: 'index_users_on_user_code', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "user_code"
+    t.string "username"
+    t.string "password_digest"
+    t.string "role"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_code"], name: "index_users_on_user_code", unique: true
   end
+
 end
