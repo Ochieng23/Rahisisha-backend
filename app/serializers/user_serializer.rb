8616e1729ebb38cb33133992dc69e2dc15f7b0f0
Abdu <abdulrahman.omar@student.moringaschool.com>
2023-04-26 18:07:08 +0300

    adds associations

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,4 +2,7 @@
 
 class UserSerializer < ActiveModel::Serializer
   attributes :user_code, :username, :role
+  has_one :employer, foreign_key: :user_code
+  has_one :seeker, foreign_key: :user_code
+  has_one :admin, foreign_key: :user_code
 end
